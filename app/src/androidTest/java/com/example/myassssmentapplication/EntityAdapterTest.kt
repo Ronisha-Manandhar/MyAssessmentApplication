717621015c85926ccfd69709d2b055863aa503c7
Ronisha-Manandhar <your_email@example.com>
2025-07-19 23:19:46 +1000

    test(android): add instrumentation tests for UI flows

--- app/src/androidTest/java/com/example/myassssmentapplication/EntityAdapterTest.kt
+++ app/src/androidTest/java/com/example/myassssmentapplication/EntityAdapterTest.kt
@@ -10,7 +10,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class `Entity(RecyclerView)AdapterTest` {
+class EntityAdapterTest {
 
     @Test
     fun `onBindViewHolderdisplayscorrectsummary`() {
